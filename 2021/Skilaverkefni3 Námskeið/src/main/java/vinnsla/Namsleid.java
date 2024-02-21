ee5cc171dabed5b4a7085f89aa06ca42e65b1cd0
Ebba Þóra Hvannberg <ebba@hi.is>
2024-02-21 16:54:22 +0000

    Fyrsta útgáfa

---
+++ 2021/Skilaverkefni3 Námskeið/src/main/java/vinnsla/Namsleid.java
@@ -0,0 +1,19 @@
+package vinnsla;
+
+/******************************************************************************
+ *  Nafn    : Ebba Þóra Hvannberg
+ *  T-póstur: ebba@hi.is
+ *
+ *  Lýsing  : Gagnatag fyrir Námsleið
+ *
+ *****************************************************************************/
+public enum Namsleid {
+    NFVS11, // prufugögn
+    NFVS12,
+    NFVS21,
+    NFVS22,
+    NHEI11,
+    NHEI12,
+    NHEI21,
+    NHEI22
+}
