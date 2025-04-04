68727de58d1b3b70ca4a92a4bf35749e174fac3f
stefansteinar04 <51232653+stefansteinar04@users.noreply.github.com>
2025-04-04 09:27:45 +0000

    Delete 2025/Verkefni 4 Viðtalsþjálfi directory

--- 2025/Verkefni 4 Viðtalsþjálfi/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module hi.interview {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires okhttp3;
-
-
-    exports hi.interview.vidmot;
-    opens hi.interview.vidmot to javafx.fxml;
-}
